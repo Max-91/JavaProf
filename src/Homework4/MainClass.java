d50eda347876a4dbe95cd608c297ec118009fabc
Max <Maximkot@mail.ru>
2020-11-03 03:55:26 +0300

    Homework 4 (first part)

---
+++ src/Homework4/MainClass.java
@@ -0,0 +1,10 @@
+package Homework4;
+/*
+ Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС).
+ Используйте wait/notify/notifyAll.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
