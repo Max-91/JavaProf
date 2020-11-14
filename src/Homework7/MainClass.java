7f33f2a830f3ea3ae8b4562c921ee75deeff5a46
Max <Maximkot@mail.ru>
2020-11-14 18:32:30 +0300

    Homework 7

--- src/Homework7/MainClass.java
+++ src/Homework7/MainClass.java
@@ -1,4 +1,8 @@
 package Homework7;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /*
 Создать класс, который может выполнять «тесты». В качестве тестов выступают классы с наборами методов
  с аннотациями @Test. Для этого у него должен быть статический метод start(), которому в качестве
@@ -11,4 +15,64 @@ package Homework7;
       иначе необходимо бросить RuntimeException при запуске «тестирования».
  */
 public class MainClass {
+    public static void main(String[] args) {
+        try {
+            start(TestClass.class);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void start(Class cl) throws InvocationTargetException, IllegalAccessException {
+        Method[] methods = cl.getDeclaredMethods();
+        // Проверка на кол-во BeforeSuite и AfterSuite
+        // и еще проверка на корректность приоритетов от 0 до 10
+        int countBefore = 0;
+        int countAfter = 0;
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
+                countBefore++;
+            }
+            if (m.isAnnotationPresent(AfterSuite.class)) {
+                countAfter++;
+            }
+            if (m.isAnnotationPresent(Test.class)) {
+                if (m.getAnnotation(Test.class).priority() < 0 | m.getAnnotation(Test.class).priority() > 10) {
+                    throw new RuntimeException("Неправильно указан приоритет у аннотации @Test (должен быть от 0 до 10)");
+                }
+            }
+        }
+        if (countBefore > 1) {
+            throw new RuntimeException("Более одного @BeforeSuite ");
+        }
+        if (countAfter > 1) {
+            throw new RuntimeException("Более одного @AfterSuite");
+        }
+        // Выполнение @BeforeSuite
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
+                m.invoke(null);
+            }
+        }
+
+        // Выполнение тестов по приоритетам
+        for (int i = 0; i <= 10; i++) {
+            for (Method m : methods) {
+                if (m.isAnnotationPresent(Test.class)) {
+                    if (m.getAnnotation(Test.class).priority() == i) {
+                        m.invoke(null);
+                    }
+                }
+            }
+        }
+        // Выполнение @AfterSuite
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(AfterSuite.class)) {
+                m.invoke(null);
+            }
+        }
+
+    }
 }
--- src/Homework7/TestClass.java
+++ src/Homework7/TestClass.java
@@ -1,4 +1,44 @@
 package Homework7;
 
 public class TestClass {
+
+    @BeforeSuite
+    public static void startTest() {
+        System.out.println("Начала тестирования");
+    }
+
+    @AfterSuite
+    public static void endTest() {
+        System.out.println("Конец тестирования");
+    }
+
+    @Test(priority = 1)
+    public static void test1() {
+        System.out.println("Тест 1");
+    }
+
+    @Test(priority = 2)
+    public static void test2() {
+        System.out.println("Тест 2");
+    }
+
+    @Test(priority = 3)
+    public static void test31() {
+        System.out.println("Тест 3.1");
+    }
+
+    @Test(priority = 3)
+    public static void test32() {
+        System.out.println("Тест 3.2");
+    }
+
+    @Test(priority = 4)
+    public static void test4() {
+        System.out.println("Тест 4");
+    }
+
+    @Test(priority = 10)
+    public static void test10() {
+        System.out.println("Тест 10");
+    }
 }
